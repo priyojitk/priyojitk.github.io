0f3a288ca8cbda744e81a3c2eb1161b5ee6a1046
Priyojit <priyojitkharibam@gmail.com>
2022-11-16 23:02:20 +0530

    update nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-light border-bottom">
+    <nav className="navbar navbar-light border-bottom container">
       <a className="navbar-brand" href="/">
         Priyojit Kharibam
       </a>
