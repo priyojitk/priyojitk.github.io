54393e81ca260051151c5bf7bc79665dfbc2ef5c
Priyojit <priyojitkharibam@gmail.com>
2022-11-16 22:04:25 +0530

    Updates

---
+++ precache-manifest.b80e7ce1a51dd4012ce71ea88317271e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb7ac9dc0e4282116156",
+    "url": "/static/css/main.2e7c2646.chunk.css"
+  },
+  {
+    "revision": "bb7ac9dc0e4282116156",
+    "url": "/static/js/main.bb7ac9dc.chunk.js"
+  },
+  {
+    "revision": "6d764ae1baf2f2355f97",
+    "url": "/static/css/1.37ef2deb.chunk.css"
+  },
+  {
+    "revision": "6d764ae1baf2f2355f97",
+    "url": "/static/js/1.6d764ae1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1b6f3b90355f1c44c0a3c84b1ce6b66",
+    "url": "/index.html"
+  }
+];
